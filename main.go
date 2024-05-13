4cc3b72961f97a5d97ddd1ec3e5b4a4520290f6d
marcusp619 <marcusp619@gmail.com>
2024-05-12 21:50:45 -0400

    feat: add init setup and helloHandler

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintf(w, "hello!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/form", helloHandler)
+
+	fmt.Println("Starting server at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
